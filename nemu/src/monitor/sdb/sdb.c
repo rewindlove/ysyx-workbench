26604b077425cd71172a16faa669421f8d32c7a5
tracer-ysyx <tracer@ysyx.org>
2023-12-24 17:53:10 +0800

    > compile NEMU ysyx_22040000 刘帅 Linux lius-vm 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:10 up  2:12,  1 user,  load average: 0.29, 0.19, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,16 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args)
+{
+	int step = 0;
+	if(args == NULL)
+					step=1;
+	else
+					sscanf(args,"%d",&step);
+	cpu_exec(step);
+	return 0;
+}
 static int cmd_help(char *args);
 
 static struct {
@@ -62,6 +72,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+	{ "si [N]" ,"单步执行",cmd_si},
 
   /* TODO: Add more commands */
 
