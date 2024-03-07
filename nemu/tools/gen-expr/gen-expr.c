4e65aa68bab84f63d1e965fb6ef8f138e7810aac
tracer-ysyx <tracer@ysyx.org>
2024-03-07 10:18:57 +0800

    > compile NEMU ysyx_22040000 刘帅 Linux lius-vm 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:18:57 up 21 min,  1 user,  load average: 0.04, 0.01, 0.01

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -33,7 +33,7 @@ static char *code_format =
 int index_buf=0;
 int choose(int n){
 		int flag = rand()%3;
-		printf("index_buf=%d,flag=%d",index_buf,flag);
+		printf("index_buf=%d,flag=%d\n",index_buf,flag);
 		return flag;
 }
 void gen_num(){
@@ -80,34 +80,35 @@ static void gen_rand_expr() {
 }
 
 int main(int argc, char *argv[]) {
-  int seed = time(0);
-  srand(seed);
-  int loop = 1;
-  if (argc > 1) {
-    sscanf(argv[1], "%d", &loop);
-  }
-  int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+				 int seed = time(0);
+    srand(seed);
+    int loop = 1;
+    if (argc > 1) {
+	sscanf(argv[1], "%d", &loop);
+    }
+    int i;
+    for (i = 0; i < loop; i ++) {
+	gen_rand_expr();
+	buf[index_buf] = '\0';
+	sprintf(code_buf, code_format, buf);
 
-    sprintf(code_buf, code_format, buf);
+	FILE *fp = fopen("/tmp/.code.c", "w");
+	assert(fp != NULL);
+	fputs(code_buf, fp);
+	fclose(fp);
 
-    FILE *fp = fopen("/tmp/.code.c", "w");
-    assert(fp != NULL);
-    fputs(code_buf, fp);
-    fclose(fp);
+	int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+	if (ret != 0) continue;
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+	fp = popen("/tmp/.expr", "r");
+	assert(fp != NULL);
 
-    fp = popen("/tmp/.expr", "r");
-    assert(fp != NULL);
+	int result;
+	ret = fscanf(fp, "%d", &result);
+	pclose(fp);
 
-    int result;
-    ret = fscanf(fp, "%d", &result);
-    pclose(fp);
-
-    printf("%u %s\n", result, buf);
-  }
-  return 0;
+	printf("%u %s\n", result, buf);
+	index_buf = 0;
+    }
+    return 0;
 }
